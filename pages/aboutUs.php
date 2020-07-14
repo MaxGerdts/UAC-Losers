08ada0dfd9b1db2313184ee1344bde27d468388a
Max Gerdts <max.gerdts95@hotmail.com>
2020-07-14 14:31:39 -0500

    chao bootstrap

--- pages/aboutUs.php
+++ pages/aboutUs.php
@@ -4,8 +4,17 @@
     <meta charset="utf-8">
     <title>Sobre nosotros</title>
     <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
   </head>
   <body>
+    <div class="container">
+      <div class="content">
+        <ul class="list-group">
+          <li>Menú</li>
+          <li>Salir</li>
+        </ul>
+      </div>
+    </div>
     <div class="container">
       hola
     </div>
