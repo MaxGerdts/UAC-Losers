cb3ecbe1883190002b0c6f4c0d8f2edee0960e49
Max Gerdts <max.gerdts95@hotmail.com>
2020-07-14 14:33:00 -0500

    index

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <title></title>
   </head>
   <body>
@@ -12,7 +11,5 @@
     </div>
 
   <script  src="assets/js/jquery-3.5.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-  <script  src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
