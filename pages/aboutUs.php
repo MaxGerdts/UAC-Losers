8a27085199da50898b5cfcecd0536b0e916052dd
Max Gerdts <max.gerdts95@hotmail.com>
2020-07-14 13:50:28 -0500

    base styles

---
+++ pages/aboutUs.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sobre nosotros</title>
+    <link rel="stylesheet" href="../assets/css/main.css">
+  </head>
+  <body>
+    <div class="container">
+      hola
+    </div>
+
+  </body>
+</html>
