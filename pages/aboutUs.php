128014f4cce00896b793db28074b0f5f5d5d2efc
Max Gerdts <max.gerdts95@hotmail.com>
2020-07-14 20:16:56 -0500

    base styles

--- pages/aboutUs.php
+++ pages/aboutUs.php
@@ -9,11 +9,15 @@
     <div class="container">
       <div class="content">
         <ul class="flex list list-primary">
-          <li class="list-items"><a class="link link-primary" href="#">Acerca de</a></li>
+          <li class="list-items"><a class="btn btn-primary link" href="#">Acerca de</a></li>
           <li class="list-items">Salir</li>
         </ul>
-        <input class="btn" type="text" name="" value="">
-        <input class="btn" type="text" name="" value="">
+        <input class="input input-primary" type="text" name="" value="">
+        <input class="input" type="text" name="" value="">
+        <p class="btn btn-primary">boton</p>
+        <span class="btn btn-primary">boton</span>
+        <input class="btn btn-primary" type="submit" name="" value="boton">
+        <a class="btn btn-primary link" href="#">Acerca de</a>
       </div>
     </div>
     <div class="container">
