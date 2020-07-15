80587d88cbe0852d7cb3d87a2e658014f461c2d6
Max Gerdts <max.gerdts95@hotmail.com>
2020-07-15 17:46:48 -0500

    profile v1

---
+++ includes/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Porn Social</title>
+  <!--<link rel="icon" type="image/png" href="../assets/img/icons/logo.png">-->
+  <link rel="stylesheet" href="../assets/css/main.css">
+</head>
---
+++ pages/profile.php
@@ -0,0 +1,41 @@
+<?php include('../includes/head.php'); ?>
+
+  <body>
+    <div class="main">
+      <div class="container flex">
+        <div class="content">
+
+        </div>
+        <div class="content">
+          <ul class="flex list">
+            <li class="list-items"><a href="#">Inicio</a></li>
+            <li class="list-items"><a href="#">Descubre</a></li>
+            <li class="list-items"><a href="#">En vivo</a></li>
+          </ul>
+
+        </div>
+        <div class="content">
+
+        </div>
+      </div>
+      <div class="container container-primary">
+        <div class="content content-secondary content-profile">
+          <img src="../assets/img/Lanhita.jpg" alt="">
+          <p class="font-lg text-center">Lanita</p>
+        </div>
+        <div class="content content-secondary content-profile bt">
+          <ul class="list flex">
+            <li class="list-items pointer">Biografía</li>
+            <li class="list-items pointer">Información</li>
+            <li class="list-items pointer">Packs</li>
+            <li class="list-items pointer">Chats hots</li>
+            <li class="list-items"><a class="btn btn-primary" href="#">Editar perfil</a></li>
+          </ul>
+        </div>
+        <div class="content content-secondary content-profile content-info">
+          info
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
