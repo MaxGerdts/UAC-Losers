b25129d798726c2eb4c8ebe7f4ac697e23b177b6
Max Gerdts <max.gerdts95@hotmail.com>
2020-07-16 12:38:40 -0500

    funcion show/hide profile

--- pages/profile.php
+++ pages/profile.php
@@ -2,7 +2,7 @@
 
   <body>
     <div class="main">
-      <div class="container flex">
+      <div class="container container-secondary flex">
         <div class="content">
 
         </div>
@@ -18,13 +18,13 @@
 
         </div>
       </div>
-      <div class="container container-primary">
-        <div class="content content-secondary content-profile">
+      <div class="container">
+        <div class="content content-secondary mw-700">
           <img src="../assets/img/Lanhita.jpg" alt="">
           <p class="font-lg text-center">Lanita</p>
         </div>
-        <div class="content content-secondary content-profile bt">
-          <ul class="list flex">
+        <div class="content content-secondary mw-700 bt">
+          <ul class="list flex menu-profile">
             <li class="list-items pointer">Biografía</li>
             <li class="list-items pointer">Información</li>
             <li class="list-items pointer">Packs</li>
@@ -32,10 +32,24 @@
             <li class="list-items"><a class="btn btn-primary" href="#">Editar perfil</a></li>
           </ul>
         </div>
-        <div class="content content-secondary content-profile content-info">
-          info
+      </div>
+      <div class="container">
+        <div class="content content-secondary mw-700 content-info">
+          Biografía
+        </div>
+        <div class="content content-secondary mw-700 content-info">
+          Informacion
+        </div>
+        <div class="content content-secondary mw-700 content-info">
+          Packs
+        </div>
+        <div class="content content-secondary mw-700 content-info">
+          Chats hots
         </div>
       </div>
     </div>
+
+    <script src="../assets/js/jquery-3.5.1.min.js"></script>
+    <script src="../assets/js/main.js"></script>
   </body>
 </html>
